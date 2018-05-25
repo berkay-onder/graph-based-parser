4746dc8d523aa9649da5cedc5c580a4dead41bc1
bonder17 <bonder17@login02.kuacc.ku.edu.tr>
2018-05-25 22:13:31 +0300

    algorithms

---
+++ parsers/index.jl
@@ -0,0 +1,27 @@
+using PyCall
+include("CKY.jl")
+include("Eisner.jl")
+unshift!(PyVector(pyimport("sys")["path"]), "")
+@pyimport mst
+
+function eisner(lambda)
+    headsize = size(lambda,2)-1
+    heads = Array{Int}(headsize)
+    score, arcset = Eisner(size(lambda,2)-1, lambda)
+    for arc in arcset
+        heads[arc[2]]=arc[1]
+    end
+    heads
+end
+
+function cky(lambda)
+    headsize = size(lambda,2)-1
+    heads = Array{Int}(headsize)
+    score, arcset = CKY(size(lambda,2)-1, lambda)
+    for arc in arcset
+        heads[arc[2]]=arc[1]
+    end
+    heads
+end
+
+edmonds(lambda) = mst.mst(lambda')[2:end]
